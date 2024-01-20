e7320bd2156032c79e5da44e52ad50f7f3e41204
BalaurZmeu <balaur.zmeu@gmail.com>
2024-01-20 15:30:38 +0200

    Started to modify the appearance of the models on the admin page

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
 
 
-admin.site.register(Author)
+# admin.site.register(Author)
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+    
+admin.site.register(Author, AuthorAdmin)
+
 admin.site.register(Genre)
 admin.site.register(Book)
 admin.site.register(BookInstance)
