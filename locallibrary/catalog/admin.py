d5b38aa2354f97cb6e07ded3a35cde3fbc98d8f7
BalaurZmeu <balaur.zmeu@gmail.com>
2024-01-20 18:03:19 +0200

    Modified the way the models are registered in the

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -3,13 +3,20 @@ from .models import Author, Genre, Book, BookInstance, Language
 
 
 # admin.site.register(Author)
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
 
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     pass
-    
-admin.site.register(Author, AuthorAdmin)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
 
 admin.site.register(Genre)
-admin.site.register(Book)
-admin.site.register(BookInstance)
 admin.site.register(Language)
