95d3e94a81ec688122cd7f5eddbf5104202ec8e0
BalaurZmeu <balaur.zmeu@gmail.com>
2024-01-21 12:07:34 +0200

    Changed how the models are displayed on the admin page

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -8,15 +8,28 @@ from .models import Author, Genre, Book, BookInstance, Language
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'author', 'display_genre')
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('status', 'due_back')
+    
+    list_display = ('book', 'status', 'due_back')
+    
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
 admin.site.register(Genre)
 admin.site.register(Language)
