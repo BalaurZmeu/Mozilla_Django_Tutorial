30fd964d7bcd4dbafd56666c1135bfd03ab2171d
BalaurZmeu <balaur.zmeu@gmail.com>
2024-01-23 16:01:12 +0200

    Modified views and index page to show genre and books with the word mind

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -14,12 +14,16 @@ def index(request):
     
     # The 'all()' is implied by default
     num_authors = Author.objects.count()
+    num_genres = Genre.objects.all().count()
+    num_mindbooks = Book.objects.filter(title__icontains='mind').count()
     
     context = {
         'books' : num_books,
         'instances' : num_instances,
         'available' : num_instances_available,
         'authors' : num_authors,
+        'genres' : num_genres,
+        'mindbooks' : num_mindbooks,
     }
     
     # Render the HTML template index.html with the data in the context variable
