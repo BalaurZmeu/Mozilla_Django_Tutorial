a2797c11a51ef11cb216a7881d9241dad1b7d64b
BalaurZmeu <balaur.zmeu@gmail.com>
2024-01-24 16:33:24 +0200

    Added view and template for book list

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Author, Book, BookInstance, Genre
+from django.views import generic
 
 
 def index(request):
@@ -28,3 +29,7 @@ def index(request):
     
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
