7327ea2ad35a9b6afcddcaf4618b465cc810828c
BalaurZmeu <balaur.zmeu@gmail.com>
2024-05-10 19:09:59 +0300

    Added tests for the view that shows bookinstances loaned by user

--- locallibrary/catalog/tests/test_views.py
+++ locallibrary/catalog/tests/test_views.py
@@ -45,3 +45,95 @@ class AuthorListViewTest(TestCase):
         self.assertTrue(response.context['is_paginated'] == True)
         self.assertEqual(len(response.context['author_list']), 3)
         
+class LoanedBookInstancesByUserListViewTest(TestCase):
+    def setUp(self):
+        test_user1 = User.objects.create_user(username='testuser1', password='123write')
+        test_user2 = User.objects.create_user(username='testuser2', password='321write')
+        test_user1.save()
+        test_user2.save()
+        
+        test_author = Author.objects.create(
+            first_name = 'John',
+            last_name = 'Smith',
+        )
+        test_genre = Genre.objects.create(name='Fantasy')
+        test_language = Language.objects.create(name='English')
+        test_book = Book.objects.create(
+            title='Book Title',
+            summary='My book summary',
+            isbn='1234567891234',
+            author=test_author,
+            language=test_language,
+        )
+        
+        genre_objects_for_book = Genre.objects.all()
+        test_book.genre.set(genre_objects_for_book)
+        test_book.save()
+        
+        number_of_book_copies = 30
+        for book_copy in range(number_of_book_copies):
+            return_date = timezone.localtime() + datetime.timedelta(days=book_copy%5)
+            the_borrower = test_user1 if book_copy % 2 else test_user2
+            status = 'm'
+            BookInstance.objects.create(
+                book=test_book,
+                imprint='Unlikely Imprint, 2016',
+                due_back=return_date,
+                borrower=the_borrower,
+                status=status,
+            )
+    
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get(reverse('my-borrowed'))
+        self.assertRedirects(response, '/accounts/login/?next=/catalog/mybooks/')
+        
+    def test_logged_in_uses_correct_template(self):
+        login = self.client.login(username='testuser1', password='123write')
+        response = self.client.get(reverse('my-borrowed'))
+        self.assertEqual(str(response.context['user']), 'testuser1')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'catalog/bookinstance_list_borrowed_user.html')
+        
+    def test_only_borrowed_books_in_list(self):
+        login = self.client.login(username='testuser1', password='123write')
+        response = self.client.get(reverse('my-borrowed'))
+        self.assertEqual(str(response.context['user']), 'testuser1')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('bookinstance_list' in response.context)
+        self.assertEqual(len(response.context['bookinstance_list']), 0)
+
+        books = BookInstance.objects.all()[:10]
+
+        for book in books:
+            book.status = 'o'
+            book.save()
+
+        response = self.client.get(reverse('my-borrowed'))
+        self.assertEqual(str(response.context['user']), 'testuser1')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('bookinstance_list' in response.context)
+
+        for bookitem in response.context['bookinstance_list']:
+            self.assertEqual(response.context['user'], bookitem.borrower)
+            self.assertEqual(bookitem.status, 'o')
+
+    def test_pages_ordered_by_due_date(self):
+        for book in BookInstance.objects.all():
+            book.status='o'
+            book.save()
+
+        login = self.client.login(username='testuser1', password='123write')
+        response = self.client.get(reverse('my-borrowed'))
+
+        self.assertEqual(str(response.context['user']), 'testuser1')
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(len(response.context['bookinstance_list']), 10)
+
+        last_date = 0
+        for book in response.context['bookinstance_list']:
+            if last_date == 0:
+                last_date = book.due_back
+            else:
+                self.assertTrue(last_date <= book.due_back)
+                last_date = book.due_back
